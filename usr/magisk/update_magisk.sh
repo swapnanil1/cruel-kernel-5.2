44e416ebb4b49d56f680f57a23078fdd7033ad14
Swapnanil <135691331+swapnanil1@users.noreply.github.com>
2023-11-20 01:59:06 +0530

    Update update_magisk.sh to 26.4

--- usr/magisk/update_magisk.sh
+++ usr/magisk/update_magisk.sh
@@ -9,18 +9,18 @@ ver="$(cat "$DIR/magisk_version" 2>/dev/null || echo -n 'none')"
 if [ "x$1" = "xcanary" ]
 then
 	nver="canary"
-	magisk_link="https://github.com/topjohnwu/magisk-files/raw/${nver}/app-debug.apk"
+	magisk_link="https://github.com/topjohnwu/Magisk/releases/download/v26.4/Magisk-v26.4.apk"
 elif [ "x$1" = "xalpha" ]
 then
 	nver="alpha"
-	magisk_link="https://github.com/vvb2060/magisk_files/raw/${nver}/app-release.apk"
+	magisk_link="https://github.com/topjohnwu/Magisk/releases/download/v26.4/Magisk-v26.4.apk"
 else
 	if [ "x$1" = "x" ]; then
 		nver="$(curl -s https://github.com/topjohnwu/Magisk/releases | grep -m 1 -Poe 'Magisk v[\d\.]+' | cut -d ' ' -f 2)"
 	else
 		nver="$1"
 	fi
-	magisk_link="https://github.com/topjohnwu/Magisk/releases/download/${nver}/Magisk-${nver}.apk"
+	magisk_link="https://github.com/topjohnwu/Magisk/releases/download/v26.4/Magisk-v26.4.apk"
 fi
 
 if [ \( -n "$nver" \) -a \( "$nver" != "$ver" \) -o ! \( -f "$DIR/magiskinit" \) -o \( "$nver" = "canary" \) -o \( "$nver" = "alpha" \) ]
